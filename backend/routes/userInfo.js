d6f02105b9336506921f82e64196e4c4568940e1
dylancochran <dylancochran@csus.edu>
2024-04-24 14:40:25 -0700

    Testing frontend-backend communications

--- backend/routes/userInfo.js
+++ backend/routes/userInfo.js
@@ -1,4 +1,9 @@
 const express = require("express")
 const router = express.Router()
 
+
+router.get('/', (req, res) => {
+    res.send('this is crazy tyler, when i click the sparkles it sends the message from the backend')
+})
+
 module.exports = router
