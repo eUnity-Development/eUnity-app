acbf4db73a5723c78ba1ff6dec288d92cd537771
tjudtmartine <145167310+tjudtmartine@users.noreply.github.com>
2024-04-27 12:58:10 -0700

    Update userInfo.js

--- backend/routes/userInfo.js
+++ backend/routes/userInfo.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 
 router.get('/', (req, res) => {
-    res.send('this is crazy tyler, when i click the sparkles it sends the message from the backend')
+    res.send('testing server response')
 })
 
 module.exports = router
