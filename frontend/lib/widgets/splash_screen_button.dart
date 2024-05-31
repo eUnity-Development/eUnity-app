23fd2aa3f05cd7104f70d17680b2afd11e3e2ddb
rahulio96 <rahgupta96@gmail.com>
2024-05-30 18:16:32 -0700

    Buttons can have any content inside

--- frontend/lib/widgets/splash_screen_button.dart
+++ frontend/lib/widgets/splash_screen_button.dart
@@ -1,26 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreenButton extends StatelessWidget {
-  final String? text;
+
   final Color color;
   final VoidCallback onTap;
   final Color borderColor;
-  final Color textColor;
   final double height;
   final double width;
-  final SvgPicture? svgIcon;
+  final Widget buttonContent;
 
   const SplashScreenButton({
     super.key,
-    this.text,
     required this.color,
     required this.onTap,
-    required this.textColor,
     required this.borderColor,
     required this.height,
     required this.width,
-    this.svgIcon,
+    required this.buttonContent,
   });
 
   @override
@@ -38,14 +34,7 @@ class SplashScreenButton extends StatelessWidget {
           width: width,
           child: Center(
             // We can reuse widget for both svg and text
-            child: text != null ? Text(
-              text!,
-              style: TextStyle(
-                color: textColor,
-                fontSize: 21,
-                fontWeight: FontWeight.w700,
-              ),
-            ) : svgIcon,
+            child: buttonContent,
           ),
         ),
       ),
