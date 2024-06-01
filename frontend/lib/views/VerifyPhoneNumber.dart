0d7a3982e6ef45ea73fa523307e130d19f84e2da
rahulio96 <rahgupta96@gmail.com>
2024-05-31 19:47:30 -0700

    Input field matches figma

--- frontend/lib/views/VerifyPhoneNumber.dart
+++ frontend/lib/views/VerifyPhoneNumber.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
+import 'package:frontend/classes/DesignVariables.dart';
 import 'package:frontend/widgets/TopBars/PushedScreenTopBar.dart';
   
 class VerifyPhoneNumber extends StatefulWidget {
@@ -18,21 +19,28 @@ class _VerifyPhoneNumber extends State<VerifyPhoneNumber> {
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: const PushedScreenTopBar(),
-        body:     OtpTextField(
-        numberOfFields: 5,
-        borderColor: Color(0xFF512DA8),
-        //set to true to show as box or false to show as dash
-        showFieldAsBox: true, 
-        //runs when a code is typed in
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: const PushedScreenTopBar(),
+      body:  OtpTextField(
+        numberOfFields: 6,
+        margin: EdgeInsets.only(right: (10/430) * screenWidth, left: (10/430) * screenWidth),
+        fieldWidth: (46/430) * screenWidth,
+        focusedBorderColor: DesignVariables.primaryRed,
+        enabledBorderColor: DesignVariables.primaryRed,
+        cursorColor: DesignVariables.greyLines,
+        showFieldAsBox: false, 
+
+        // runs when a code is typed in
         onCodeChanged: (String code) {
-            //handle validation or checks here           
+            // handle validation or checks here           
         },
-        //runs when every textfield is filled
+
+        // runs when every textfield is filled
         onSubmit: (String verificationCode){
             showDialog(
                 context: context,
@@ -43,7 +51,7 @@ class _VerifyPhoneNumber extends State<VerifyPhoneNumber> {
                 );
                 }
             );
-        }, // end onSubmit
-    ),
-      );
+        },
+      ),
+    );
   }}
