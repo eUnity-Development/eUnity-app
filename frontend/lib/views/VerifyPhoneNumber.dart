99c6b806dfac14d0b772386238c9b3b25a4b9d99
rahulio96 <rahgupta96@gmail.com>
2024-05-31 20:44:37 -0700

    Button and text to match figma

--- frontend/lib/views/VerifyPhoneNumber.dart
+++ frontend/lib/views/VerifyPhoneNumber.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
 import 'package:frontend/classes/DesignVariables.dart';
+import 'package:frontend/widgets/LoginSignup/login_signup_button.dart';
 import 'package:frontend/widgets/TopBars/PushedScreenTopBar.dart';
   
 class VerifyPhoneNumber extends StatefulWidget {
@@ -19,6 +20,10 @@ class _VerifyPhoneNumber extends State<VerifyPhoneNumber> {
     super.dispose();
   }
 
+  void resend() async {
+    print("clicked resend");
+  }
+
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
@@ -26,32 +31,95 @@ class _VerifyPhoneNumber extends State<VerifyPhoneNumber> {
 
     return Scaffold(
       appBar: const PushedScreenTopBar(),
-      body:  OtpTextField(
-        numberOfFields: 6,
-        margin: EdgeInsets.only(right: (10/430) * screenWidth, left: (10/430) * screenWidth),
-        fieldWidth: (46/430) * screenWidth,
-        focusedBorderColor: DesignVariables.primaryRed,
-        enabledBorderColor: DesignVariables.primaryRed,
-        cursorColor: DesignVariables.greyLines,
-        showFieldAsBox: false, 
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: (40/932) * screenHeight,
+          ),
 
-        // runs when a code is typed in
-        onCodeChanged: (String code) {
-            // handle validation or checks here           
-        },
+          const Text(
+            'Enter 6 Digit Code',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 24,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
 
-        // runs when every textfield is filled
-        onSubmit: (String verificationCode){
-            showDialog(
-                context: context,
-                builder: (context){
-                return AlertDialog(
-                    title: Text("Verification Code"),
-                    content: Text('Code entered is $verificationCode'),
+          SizedBox(
+            height: (40/932) * screenHeight,
+          ),
+
+          OtpTextField(
+            numberOfFields: 6,
+            margin: EdgeInsets.symmetric(horizontal: (10/430) * screenWidth),
+            fieldWidth: (46/430) * screenWidth,
+            focusedBorderColor: DesignVariables.primaryRed,
+            enabledBorderColor: DesignVariables.primaryRed,
+            cursorColor: DesignVariables.greyLines,
+            showFieldAsBox: false, 
+
+            // runs when a code is typed in
+            onCodeChanged: (String code) {
+                // handle validation or checks here           
+            },
+
+            // runs when every textfield is filled
+            onSubmit: (String verificationCode){
+                showDialog(
+                    context: context,
+                    builder: (context){
+                    return AlertDialog(
+                        title: Text("Verification Code"),
+                        content: Text('Code entered is $verificationCode'),
+                    );
+                    }
                 );
-                }
-            );
-        },
-      ),
+            },
+          ),
+
+          SizedBox(
+            height: (38/932) * screenHeight,
+          ),
+
+          const Padding (
+            padding: EdgeInsets.symmetric(horizontal: 21),
+              child: Text(
+                'We sent your phone number +1(111)111-1111 a 6 digit code. ' 
+                'Please enter it here for verification purposes.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+          ),
+
+          SizedBox(
+            height: (23/932) * screenHeight,
+          ),
+
+          LoginSignupButton(
+            color: DesignVariables.primaryRed,  
+            onTap: resend, 
+            borderColor: Colors.transparent, 
+            width: (334.67/430) * screenWidth, 
+            height: (52/932) * screenHeight, 
+            buttonContent: const Text(
+              'Re-send Verification Code',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w700,
+                ),
+            )
+          )
+
+
+        ]
+      )  
+      
     );
   }}
