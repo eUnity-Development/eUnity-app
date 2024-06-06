e3b56d62eaafc20ade26341fca4c23f30e0f4c77
rahulio96 <rahgupta96@gmail.com>
2024-06-05 21:08:01 -0700

    Name textbox and header

--- frontend/lib/views/NameDOBGender.dart
+++ frontend/lib/views/NameDOBGender.dart
@@ -1,3 +1,4 @@
+import 'package:eunity/classes/DesignVariables.dart';
 import 'package:eunity/widgets/TopBars/PushedScreenTopBar.dart';
 import 'package:flutter/material.dart';
 
@@ -9,16 +10,72 @@ class NameDOBGender extends StatefulWidget {
 }
 
 class _NameDOBGender extends State<NameDOBGender> {
-  @override
-  void initState() {
-    super.initState();
-  }
+  final TextEditingController _nameController = TextEditingController();
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PushedScreenTopBar(hasArrow: false),
+      appBar: const PushedScreenTopBar(hasArrow: false),
+      body: Padding(     
+        padding: EdgeInsets.symmetric(horizontal: 18.0), 
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Name",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
 
+            SizedBox(
+              height: 7 * DesignVariables.heightConversion,
+            ),
+
+            const Text(
+              "The name you enter here will appear on your profile.",
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+
+            SizedBox(
+              height: 30 * DesignVariables.heightConversion,
+            ),
+
+            Container(
+              height: 48 * DesignVariables.heightConversion,
+              width: 393 * DesignVariables.widthConversion,
+              child: TextField( 
+                controller: _nameController,
+                style: const TextStyle(
+                  fontSize: 14,
+                ),
+                textAlignVertical: TextAlignVertical.center,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 19 * DesignVariables.heightConversion,
+                    horizontal: 15 * DesignVariables.widthConversion
+                  ),
+                  labelText: 'First Name',
+                  labelStyle: const TextStyle(
+                    fontSize: 14,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: DesignVariables.greyLines
+                    ),
+                  ),
+                )
+              )
+            )
+
+            
+          ]
+      ))
 
     );
   }
