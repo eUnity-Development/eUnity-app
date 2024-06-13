aaa2c97936c25b2b13f028c68b6e5cbdcfe4d40e
rahulio96 <rahgupta96@gmail.com>
2024-06-12 18:06:08 -0700

    Backspace goes back if field is full

--- frontend/lib/views/NameDOBGender.dart
+++ frontend/lib/views/NameDOBGender.dart
@@ -2,6 +2,7 @@ import 'package:eunity/classes/DesignVariables.dart';
 import 'package:eunity/widgets/LoginSignup/login_signup_button_content.dart';
 import 'package:eunity/widgets/TopBars/PushedScreenTopBar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 import '../widgets/LoginSignup/login_signup_button.dart';
@@ -27,14 +28,42 @@ class _NameDOBGender extends State<NameDOBGender> {
   final TextEditingController _yearController3 = TextEditingController();
   final TextEditingController _yearController4 = TextEditingController();
 
+  final FocusNode _monthFocus1 = FocusNode();
+  final FocusNode _monthFocus2 = FocusNode();
+
+  final FocusNode _dayFocus1 = FocusNode();
+  final FocusNode _dayFocus2 = FocusNode();
+
+  final FocusNode _yearFocus1 = FocusNode();
+  final FocusNode _yearFocus2 = FocusNode();
+  final FocusNode _yearFocus3 = FocusNode();
+  final FocusNode _yearFocus4 = FocusNode();
+
   void onClick() {
     print('CLICKED!');
   }
 
   @override
   void dispose() {
+    _nameController.dispose();
     _monthController1.dispose();
     _monthController2.dispose();
+    _dayController1.dispose();
+    _dayController2.dispose();
+    _yearController1.dispose();
+    _yearController2.dispose();
+    _yearController3.dispose();
+    _yearController4.dispose();
+
+    _monthFocus1.dispose();
+    _monthFocus2.dispose();
+    _dayFocus1.dispose();
+    _dayFocus2.dispose();
+    _yearFocus1.dispose();
+    _yearFocus2.dispose();
+    _yearFocus3.dispose();
+    _yearFocus4.dispose();
+
     super.dispose();
   }
 
@@ -161,9 +190,19 @@ class _NameDOBGender extends State<NameDOBGender> {
               width: 95.53 * DesignVariables.widthConversion, 
               height: 48 * DesignVariables.heightConversion, 
               inputs: [
-                InidividualTextField(controller: _monthController1, hintText: 'M'),
+                IndividualTextField(
+                  controller: _monthController1, 
+                  hintText: 'M',
+                  focusNode: _monthFocus1,
+                  nextFocusNode: _monthFocus2,
+                ),
                 const SizedBox(width: 10),
-                InidividualTextField(controller: _monthController2, hintText: 'M')
+                IndividualTextField(
+                  controller: _monthController2, 
+                  hintText: 'M',
+                  focusNode: _monthFocus2,
+                  nextFocusNode: _dayFocus1,
+                )
               ]
             ),
 
@@ -189,9 +228,19 @@ class _NameDOBGender extends State<NameDOBGender> {
               width: 95.53 * DesignVariables.widthConversion, 
               height: 48 * DesignVariables.heightConversion, 
               inputs: [
-                InidividualTextField(controller: _dayController1, hintText: 'D'),
+                IndividualTextField(
+                  controller: _dayController1, 
+                  hintText: 'D',
+                  focusNode: _dayFocus1,
+                  nextFocusNode: _dayFocus2,
+                ),
                 const SizedBox(width: 10),
-                InidividualTextField(controller: _dayController2, hintText: 'D')
+                IndividualTextField(
+                  controller: _dayController2, 
+                  hintText: 'D',
+                  focusNode: _dayFocus2,
+                  nextFocusNode: _yearFocus1,
+                )
               ]
             ),
 
@@ -217,13 +266,32 @@ class _NameDOBGender extends State<NameDOBGender> {
               width: 127.92 * DesignVariables.widthConversion, 
               height: 48 * DesignVariables.heightConversion, 
               inputs: [
-                InidividualTextField(controller: _yearController1, hintText: 'Y'),
+                IndividualTextField(
+                  controller: _yearController1, 
+                  hintText: 'Y',
+                  focusNode: _yearFocus1,
+                  nextFocusNode: _yearFocus2,
+                ),
                 const SizedBox(width: 10),
-                InidividualTextField(controller: _yearController2, hintText: 'Y'),
+                IndividualTextField(
+                  controller: _yearController2, 
+                  hintText: 'Y',
+                  focusNode: _yearFocus2,
+                  nextFocusNode: _yearFocus3,
+                ),
                 const SizedBox(width: 10),
-                InidividualTextField(controller: _yearController3, hintText: 'Y'),
+                IndividualTextField(
+                  controller: _yearController3, 
+                  hintText: 'Y',
+                  focusNode: _yearFocus3,
+                  nextFocusNode: _yearFocus4,
+                ),
                 const SizedBox(width: 10),
-                InidividualTextField(controller: _yearController4, hintText: 'Y'),
+                IndividualTextField(
+                  controller: _yearController4, 
+                  hintText: 'Y',
+                  focusNode: _yearFocus4,
+                ),
               ]
             ), 
           ],
@@ -367,14 +435,18 @@ class DOBSection extends StatelessWidget {
   }
 }
 
-class InidividualTextField extends StatelessWidget {
+class IndividualTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
+  final FocusNode focusNode;
+  final FocusNode? nextFocusNode;
 
-  const InidividualTextField({
+  const IndividualTextField({
     super.key,
     required this.controller,
-    required this.hintText
+    required this.hintText,
+    required this.focusNode,
+    this.nextFocusNode,
   });
 
   @override
@@ -384,9 +456,11 @@ class InidividualTextField extends StatelessWidget {
       child: Stack(
         children: [
           TextField(
+            textInputAction: TextInputAction.next,
             style: const TextStyle(fontSize: 14),
             maxLength: 1, 
             controller: controller,
+            focusNode: focusNode,
             
             decoration: InputDecoration(
               contentPadding: const EdgeInsets.only(bottom: 2),
@@ -397,6 +471,14 @@ class InidividualTextField extends StatelessWidget {
 
             textAlignVertical: TextAlignVertical.center,
             textAlign: TextAlign.center,
+
+            onChanged: (value) {
+              if (value.isNotEmpty) {
+                nextFocusNode?.requestFocus();
+              } else if (value.isEmpty) {
+                focusNode.previousFocus();
+              }
+            },
 
           ),
           Positioned(
