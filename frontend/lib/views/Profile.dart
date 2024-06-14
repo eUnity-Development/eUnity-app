80dabe5a4e63f0847fc6523ee4a59295691130a0
dylancochran <dylancochran@csus.edu>
2024-06-13 19:05:12 -0700

    Report screen test

--- frontend/lib/views/Profile.dart
+++ frontend/lib/views/Profile.dart
@@ -1,6 +1,7 @@
 import 'package:eunity/classes/AuthHelper.dart';
 import 'package:eunity/views/EditProfile.dart';
 import 'package:eunity/views/LoginSignup.dart';
+import 'package:eunity/views/ReportScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:eunity/classes/DesignVariables.dart';
 import 'package:eunity/views/FeedbackScreen.dart';
@@ -48,6 +49,15 @@ class _ProfileState extends State<Profile> {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => EditProfile()),
+    );
+  }
+
+  //This realistically should not be here at all when we get to the actual place. Just a nice little place to put it
+  void navigateToReportTest() async {
+    print('clicked report');
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => ReportScreen()),
     );
   }
 
@@ -113,6 +123,29 @@ class _ProfileState extends State<Profile> {
                     ),
                   )),
               onTap: navigateToEditProfile,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            GestureDetector(
+              child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(
+                          width: 1, color: DesignVariables.greyLines),
+                      color: DesignVariables.primaryRed),
+                  height: 51,
+                  width: 207,
+                  child: Center(
+                    child: Text(
+                      "Test Report Screen",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  )),
+              onTap: navigateToReportTest,
             ),
             const SizedBox(
               height: 15,
