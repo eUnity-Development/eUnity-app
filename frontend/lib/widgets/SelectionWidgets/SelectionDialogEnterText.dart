12a5719ab74f75c485184b1c596edc8d1e9e4805
rahulio96 <rahgupta96@gmail.com>
2024-07-23 15:14:29 -0700

    Format SelectionDialogEnterText

--- frontend/lib/widgets/SelectionWidgets/SelectionDialogEnterText.dart
+++ frontend/lib/widgets/SelectionWidgets/SelectionDialogEnterText.dart
@@ -6,14 +6,11 @@ class SelectionDialogEnterText extends StatefulWidget {
   final String cacheKey;
   final String labelString;
   const SelectionDialogEnterText(
-      {super.key,
-      required this.cacheKey,
-      required this.labelString
-      });
+      {super.key, required this.cacheKey, required this.labelString});
 
   @override
-  State<SelectionDialogEnterText> createState() => _SelectionDialogEnterTextState();
-  
+  State<SelectionDialogEnterText> createState() =>
+      _SelectionDialogEnterTextState();
 }
 
 class _SelectionDialogEnterTextState extends State<SelectionDialogEnterText> {
@@ -21,56 +18,56 @@ class _SelectionDialogEnterTextState extends State<SelectionDialogEnterText> {
 
   @override
   Widget build(BuildContext context) {
-    _textController.text = UserInfoHelper.userInfoCache[widget.cacheKey] == 'Enter'
-      ? '' : UserInfoHelper.userInfoCache[widget.cacheKey];
+    _textController.text =
+        UserInfoHelper.userInfoCache[widget.cacheKey] == 'Enter'
+            ? ''
+            : UserInfoHelper.userInfoCache[widget.cacheKey];
 
-      void updateCache(String value) {
-        setState(() {
-          if (value != '') {
-            UserInfoHelper.userInfoCache[widget.cacheKey] = value;
-          } else {
-            UserInfoHelper.userInfoCache[widget.cacheKey] = 'Enter';
-          }
-        });
-      }
+    void updateCache(String value) {
+      setState(() {
+        if (value != '') {
+          UserInfoHelper.userInfoCache[widget.cacheKey] = value;
+        } else {
+          UserInfoHelper.userInfoCache[widget.cacheKey] = 'Enter';
+        }
+      });
+    }
 
     return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-            child: TextField(
-              controller: _textController,
-              onSubmitted: updateCache,
-              decoration: InputDecoration(
-                            contentPadding: EdgeInsets.symmetric(
-                              vertical: 19 * DesignVariables.heightConversion,
-                              horizontal: 15 * DesignVariables.widthConversion
-                            ),
-                            labelText: widget.labelString,
-                            labelStyle: TextStyle(
-                              color: Colors.black.withOpacity(0.5),
-                              fontSize: 18,
-                            ),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: const BorderSide(
-                                  width: 1,
-                                ),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide(
-                                  color: DesignVariables.greyLines,
-                                  width: 1,
-                                ),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide: BorderSide(
-                                  color: DesignVariables.greyLines,
-                                  width: 1,
-                                ),
-                              ),
-                          ),
-            )
-          );
+        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+        child: TextField(
+          controller: _textController,
+          onSubmitted: updateCache,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(
+                vertical: 19 * DesignVariables.heightConversion,
+                horizontal: 15 * DesignVariables.widthConversion),
+            labelText: widget.labelString,
+            labelStyle: TextStyle(
+              color: Colors.black.withOpacity(0.5),
+              fontSize: 18,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(
+                width: 1,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                color: DesignVariables.greyLines,
+                width: 1,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                color: DesignVariables.greyLines,
+                width: 1,
+              ),
+            ),
+          ),
+        ));
   }
 }
