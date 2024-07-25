2cb91ab5b16003127c101abf56a508858fbc27c9
rahulio96 <rahgupta96@gmail.com>
2024-07-24 21:20:43 -0700

    Height pop-up with scroll select

--- frontend/lib/widgets/SelectionWidgets/SelectionDialog.dart
+++ frontend/lib/widgets/SelectionWidgets/SelectionDialog.dart
@@ -1,5 +1,6 @@
 import 'package:eunity/classes/UserInfoHelper.dart';
 import 'package:eunity/widgets/SelectionWidgets/SelectionDialogEnterText.dart';
+import 'package:eunity/widgets/SelectionWidgets/SelectionDialogHeight.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -12,15 +13,15 @@ class SelectionDialog extends StatefulWidget {
   final bool multiSelect;
   final String cacheKey;
   final bool? isListLong;
-  const SelectionDialog(
-      {super.key,
-      required this.options,
-      required this.question,
-      required this.assetPath,
-      required this.multiSelect,
-      required this.cacheKey,
-      this.isListLong,
-      });
+  const SelectionDialog({
+    super.key,
+    required this.options,
+    required this.question,
+    required this.assetPath,
+    required this.multiSelect,
+    required this.cacheKey,
+    this.isListLong,
+  });
 
   @override
   State<SelectionDialog> createState() => _SelectionDialogState();
@@ -54,19 +55,20 @@ class _SelectionDialogState extends State<SelectionDialog> {
 
     bool isLongList = widget.isListLong ?? false;
 
-    double maxHeight = MediaQuery.of(context).size.height - (40 * DesignVariables.heightConversion);
+    double maxHeight = MediaQuery.of(context).size.height -
+        (40 * DesignVariables.heightConversion);
     double minHeight = 400;
     double height = (isLongList ? 32.0 : 80.0) * widget.options.length;
 
     // both job and height require user input, so make room for keyboard
-    if (widget.cacheKey == 'job' || widget.cacheKey == 'height') {
+    if (widget.cacheKey == 'job') {
       height = maxHeight / 1.25;
     }
-    
+
     if (widget.question.length > 25) {
       height += 100.0;
     }
-    
+
     if (height < minHeight) {
       height = minHeight;
     } else if (height > maxHeight) {
@@ -77,7 +79,7 @@ class _SelectionDialogState extends State<SelectionDialog> {
         height: height,
         width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
+            borderRadius: const BorderRadius.only(
                 topLeft: Radius.circular(25), topRight: Radius.circular(25)),
             color: Colors.white,
             border: Border.all(color: DesignVariables.greyLines, width: 2)),
@@ -98,7 +100,7 @@ class _SelectionDialogState extends State<SelectionDialog> {
                   },
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
               Center(
@@ -110,7 +112,8 @@ class _SelectionDialogState extends State<SelectionDialog> {
                         WidgetSpan(
                           child: Transform.translate(
                             // manually center the svg
-                            offset: Offset(0, (questionStyle.fontSize ?? 24) * 0.16),
+                            offset: Offset(
+                                0, (questionStyle.fontSize ?? 24) * 0.16),
                             child: Padding(
                               padding: const EdgeInsets.only(right: 5.0),
                               child: SvgPicture.asset(
@@ -118,85 +121,104 @@ class _SelectionDialogState extends State<SelectionDialog> {
                                 height: 31,
                                 width: 31,
                               ),
-                              ),
+                            ),
                           ),
                         ),
                       ],
                       TextSpan(
                         text: widget.question,
-                        style:questionStyle,
+                        style: questionStyle,
                       ),
                     ],
                   ),
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 20,
               ),
               SizedBox(
                 height: height - 150,
                 width: double.infinity,
-                  child: (widget.cacheKey == 'job') ? 
-                    SelectionDialogEnterText(cacheKey: widget.cacheKey, labelString: 'Job Title (Optional)',) :      
-                  Wrap(
-                    spacing: 6.0,
-                    alignment: WrapAlignment.center,
-                    children: List<Widget>.generate(widget.options.length, (index) {
-                      return buildOptionButton(
-                        index, 
-                        selectedBox, 
-                        unselectedBox, 
-                        selectedText, 
-                        unselectedText,
-                        isLongList
-                      );
-                    }),
-                  ),
-                ),
+                child: (widget.cacheKey == 'job')
+                    ? SelectionDialogEnterText(
+                        cacheKey: widget.cacheKey,
+                        labelString: 'Job Title (Optional)',
+                      )
+                    : (widget.cacheKey == 'height')
+                        ? SelectionDialogHeight(
+                            exitFunction: () => Navigator.pop(context),
+                            cacheKey: widget.cacheKey,
+                          )
+                        : Wrap(
+                            spacing: 6.0,
+                            alignment: WrapAlignment.center,
+                            children: List<Widget>.generate(
+                                widget.options.length, (index) {
+                              return buildOptionButton(
+                                  index,
+                                  selectedBox,
+                                  unselectedBox,
+                                  selectedText,
+                                  unselectedText,
+                                  isLongList);
+                            }),
+                          ),
+              ),
             ],
           ),
         ));
   }
-   Widget buildOptionButton(int index, BoxDecoration selectedBox, BoxDecoration unselectedBox, TextStyle selectedText, TextStyle unselectedText, bool isLongList) {
+
+  Widget buildOptionButton(
+      int index,
+      BoxDecoration selectedBox,
+      BoxDecoration unselectedBox,
+      TextStyle selectedText,
+      TextStyle unselectedText,
+      bool isLongList) {
     bool isSelected;
     if (widget.multiSelect) {
-      isSelected = UserInfoHelper.userInfoCache[widget.cacheKey].contains(widget.options[index]);
+      isSelected = UserInfoHelper.userInfoCache[widget.cacheKey]
+          .contains(widget.options[index]);
     } else {
-      isSelected = UserInfoHelper.userInfoCache[widget.cacheKey] == widget.options[index];
+      isSelected = UserInfoHelper.userInfoCache[widget.cacheKey] ==
+          widget.options[index];
     }
 
     return GestureDetector(
-      child: (!isLongList) ?
-        Padding(
-          padding: EdgeInsets.only(top: 15, left: 40, right: 40),
-          child: Container(
-            width: double.infinity,
-            height: 42,
-            decoration: isSelected ? selectedBox : unselectedBox,
-            child: Center(
-              child: Text(
-                widget.options[index],
-                style: isSelected ? selectedText : unselectedText,
+      child: (!isLongList)
+          ? Padding(
+              padding: EdgeInsets.only(top: 15, left: 40, right: 40),
+              child: Container(
+                width: double.infinity,
+                height: 42,
+                decoration: isSelected ? selectedBox : unselectedBox,
+                child: Center(
+                  child: Text(
+                    widget.options[index],
+                    style: isSelected ? selectedText : unselectedText,
+                  ),
+                ),
+              ),
+            )
+          : Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 1.0),
+              child: Wrap(
+                alignment: WrapAlignment.center,
+                children: [
+                  Container(
+                    decoration: isSelected ? selectedBox : unselectedBox,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 3.5),
+                    child: Text(
+                      widget.options[index],
+                      style: isSelected ? selectedText : unselectedText,
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-        ) :
-        Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 1.0),
-        child: Wrap(
-          alignment: WrapAlignment.center,
-          children: [
-            Container(
-              decoration: isSelected ? selectedBox : unselectedBox,
-              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 3.5),
-              child: Text(
-                widget.options[index],
-                style: isSelected ? selectedText : unselectedText,
-              ),
-            ),
-          ],
-        ),
-      ),
       onTap: () {
         setState(() {
           if (widget.multiSelect) {
@@ -208,7 +230,8 @@ class _SelectionDialogState extends State<SelectionDialog> {
             }
             UserInfoHelper.updateCacheVariable(widget.cacheKey, cacheValue);
           } else {
-            UserInfoHelper.updateCacheVariable(widget.cacheKey, widget.options[index]);
+            UserInfoHelper.updateCacheVariable(
+                widget.cacheKey, widget.options[index]);
           }
         });
       },
