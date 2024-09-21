3c2a59b5ff2fc8b9909c1c0a271f142a7cef851e
rahulio96 <rahgupta96@gmail.com>
2024-09-21 13:46:56 -0700

    Add upgrade button

--- frontend/lib/classes/DesignVariables.dart
+++ frontend/lib/classes/DesignVariables.dart
@@ -4,6 +4,7 @@ class DesignVariables {
   static Color primaryRed = Color.fromARGB(255, 255, 92, 92);
   static Color greyLines = Color.fromARGB(255, 217, 217, 217);
   static Color offWhite = Color.fromARGB(255, 251, 251, 251);
+  static Color gold = Color.fromARGB(255, 245, 184, 46);
 
   static late double widthConversion;
   static late double heightConversion;
--- frontend/lib/widgets/PremiumWidget/PremiumAd.dart
+++ frontend/lib/widgets/PremiumWidget/PremiumAd.dart
@@ -1,9 +1,15 @@
+import 'package:eunity/views/LoginSignup.dart';
+import 'package:eunity/widgets/LoginSignup/login_signup_button.dart';
 import 'package:flutter/material.dart';
 import 'package:eunity/classes/DesignVariables.dart';
 
 class PremiumAd extends StatelessWidget {
   const PremiumAd({super.key});
 
+  void purchase() {
+    // Go to a screen to actually purchase premium
+    print("BUY BUY BUY");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +23,27 @@ class PremiumAd extends StatelessWidget {
       child: Column(
         children: [
           SizedBox(height: 20 * DesignVariables.widthConversion),
-          Image.asset("assets/premium/premium-logo.png")
+          Image.asset("assets/premium/premium-logo.png"),
+          const Spacer(),
+          LoginSignupButton(
+            color: DesignVariables.gold, 
+            onTap: purchase, 
+            borderColor: Colors.transparent, 
+            height: 53, 
+            width: 189, 
+            buttonContent: 
+              const Text(
+                "Upgrade",
+                style: TextStyle(
+                  fontSize: 24, 
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white,
+                )
+              )
+          ),
+          SizedBox(height: 20 * DesignVariables.widthConversion),
         ],
+        
       )
     );
   }
